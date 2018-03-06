808d6e5888ca2e87e826b30075706da563177bc0
Anderson devmaker <anderson.devmaker@gmail.com>
2018-03-06 11:46:05 -0300

    open maps
    open maps

--- utils/src/main/java/br/com/andersonsoares/utils/ExtensionActivity.kt
+++ utils/src/main/java/br/com/andersonsoares/utils/ExtensionActivity.kt
@@ -10,6 +10,9 @@ import java.lang.Exception
 import android.R.attr.onClick
 import android.app.AlertDialog
 import android.content.DialogInterface
+import android.widget.Toast
+import android.content.Intent
+import android.net.Uri
 
 
 
@@ -96,3 +99,71 @@ fun Activity.showDialog(title:String,message:String,positive:String,negative:Str
         Log.e("showDialog", e.toString(), e)
     }
 }
+fun Activity.openNavigation(latitude:String,longitude:String){
+    try {
+        val uri = "google.navigation:q=$latitude,$longitude"
+        startActivity(Intent(Intent.ACTION_VIEW,
+                Uri.parse(uri)).setPackage("com.google.android.apps.maps"))
+    } catch (ex: Exception) {
+
+        try {
+            val uri = "geo:$latitude,$longitude"
+            startActivity(Intent(Intent.ACTION_VIEW,
+                    Uri.parse(uri)))
+        } catch (e2: Exception) {
+            Toast.makeText(this, "Não é possivel abrir o Google Maps.", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+}
+
+inline fun Activity.openGoogleMaps(latitude:String,longitude:String) = openGoogleMaps(latitude,longitude,"")
+
+
+
+fun Activity.openGoogleMaps(latitude:String,longitude:String,query:String){
+
+    try {
+        if(query.isEmpty()){
+            val uri = "geo:$latitude,$longitude"
+            startActivity(Intent(Intent.ACTION_VIEW,
+                    Uri.parse(uri)).setPackage("com.google.android.apps.maps"))
+        }else{
+            val uri = "geo:$latitude,$longitude?q=" + Uri.encode(query)
+
+            startActivity(Intent(Intent.ACTION_VIEW,
+                    Uri.parse(uri)).setPackage("com.google.android.apps.maps"))
+        }
+
+    } catch (ex: Exception) {
+
+        openMaps(latitude,longitude)
+
+    }
+}
+
+fun Activity.openWaze(latitude:String,longitude:String){
+
+    try {
+            val uri = "waze://?ll=$latitude,$longitude"
+            startActivity(Intent(Intent.ACTION_VIEW,
+                    Uri.parse(uri)))
+
+    } catch (ex: Exception) {
+
+        openMaps(latitude,longitude)
+
+    }
+}
+
+fun Activity.openMaps(latitude:String,longitude:String){
+    try {
+        val uri = "geo:$latitude,$longitude"
+        startActivity(Intent(Intent.ACTION_VIEW,
+                Uri.parse(uri)))
+    } catch (e2: Exception) {
+        Toast.makeText(this, "Não é possivel abrir o Waze.", Toast.LENGTH_SHORT).show()
+    }
+}
+
+
